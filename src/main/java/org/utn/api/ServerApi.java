74a437873ef52f04b1ea3c033adc9c0379e5946b
DrivasUTN <drivas@frba.utn.edu.ar>
2023-05-11 08:15:49 -0300

    Se agrega API basica que recibe un post a /bot para inicializar el bot

--- src/main/java/org/utn/api/ServerApi.java
+++ src/main/java/org/utn/api/ServerApi.java
@@ -1,2 +1,17 @@
-package org.utn.api;public class ServerApi {
+package org.utn.api;
+
+import io.javalin.Javalin;
+import org.utn.api.controllers.IniciarBotController;
+
+public class ServerApi {
+    public static void main(String[] args) {
+
+        Integer port = Integer.parseInt( System.getProperty("port", "8080"));
+        Javalin app = Javalin.create().start(port);
+
+
+        app.post("/bot", new IniciarBotController());
+
+    }
+
 }
--- src/main/java/org/utn/api/controllers/IniciarBotController.java
+++ src/main/java/org/utn/api/controllers/IniciarBotController.java
@@ -1,2 +1,23 @@
-package org.utn.api.controllers;public class IniciarBotController {
+package org.utn.api.controllers;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.utn.bot.TelegramBot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IniciarBotController implements Handler {
+    @Override
+    public void handle(Context ctx) throws Exception {
+
+        Map<String,String> rta = new HashMap<String, String>();
+        if (TelegramBot.isBotStarted()) {
+            rta.put("bot_status", "El bot ya se encontraba iniciado.");
+        } else {
+            TelegramBot.main(null);
+            rta.put("bot_status", "Se inicio la ejecución del BOT correctamente.");
+        }
+        ctx.json(rta);
+    }
 }
