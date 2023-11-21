d7bb0794d1cb7f73de12169b445c55d9f590c770
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-11-21 00:21:01 -0300

    Type y Status de AccessibilityFeature.

--- catalog-code-service/src/main/java/org/utn/domain/AccessibilityFeature.java
+++ catalog-code-service/src/main/java/org/utn/domain/AccessibilityFeature.java
@@ -1,12 +1,19 @@
 package org.utn.domain;
 
 public class AccessibilityFeature {
+    private Type type;
+    private Status status;
+    private Station station;
 
-    public enum Type {
+    private enum Type {
         ELEVATOR,
         ESCALATOR,
         RAMP,
         SIGNAGE,
     }
-    private Station station;
+
+    private enum Status {
+        FUNCTIONAL,
+        INACCESSIBLE
+    }
 }
