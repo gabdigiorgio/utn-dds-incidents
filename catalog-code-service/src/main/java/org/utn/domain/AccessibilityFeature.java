1b0c050465c2c9f876377f8489ad2a4a6c918d46
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-11-21 21:54:43 -0300

    Annotations.

--- catalog-code-service/src/main/java/org/utn/domain/AccessibilityFeature.java
+++ catalog-code-service/src/main/java/org/utn/domain/AccessibilityFeature.java
@@ -1,8 +1,17 @@
 package org.utn.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+@Entity
 public class AccessibilityFeature {
+    @Id
     private String code;
+    @Enumerated(EnumType.STRING)
     private Type type;
+    @Enumerated(EnumType.STRING)
     private Status status;
     private Station station;
 
