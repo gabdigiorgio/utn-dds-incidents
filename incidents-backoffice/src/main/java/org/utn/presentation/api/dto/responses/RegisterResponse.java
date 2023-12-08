a2b74a3ad847f1e4a33389a9ccf5f8f427170c62
Viviana Rondón <viviana.rondon97@gmail.com>
2023-12-08 16:28:48 -0300

    fix(ui): added user to navbar

--- incidents-backoffice/src/main/java/org/utn/presentation/api/dto/responses/RegisterResponse.java
+++ incidents-backoffice/src/main/java/org/utn/presentation/api/dto/responses/RegisterResponse.java
@@ -5,17 +5,21 @@ import org.utn.domain.users.User;
 public class RegisterResponse {
     private String email;
     private String token;
+    private String role;
 
     public RegisterResponse(User user) {
         this.email = user.getEmail();
         this.token = user.getToken();
+        this.role = user.getRole().toString();
     }
 
     public String getEmail() {
         return email;
     }
 
-    public String getToken() {
-        return token;
+    public String getToken() { return token; }
+    public String getRole() {
+        return role;
     }
+
 }
